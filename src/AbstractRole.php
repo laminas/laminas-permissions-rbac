9b042befe696ecd8fdba33e4def306248b4ef825
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:37:48 -0600

    Rewriting as Laminas Project package

--- src/AbstractRole.php
+++ src/AbstractRole.php
@@ -1,13 +1,12 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-permissions-rbac for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-permissions-rbac/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-permissions-rbac/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Permissions\Rbac;
+namespace Laminas\Permissions\Rbac;
 
 use RecursiveIteratorIterator;
 
@@ -87,7 +86,7 @@ abstract class AbstractRole extends AbstractIterator implements RoleInterface
         }
         if (!$child instanceof RoleInterface) {
             throw new Exception\InvalidArgumentException(
-                'Child must be a string or implement Zend\Permissions\Rbac\RoleInterface'
+                'Child must be a string or implement Laminas\Permissions\Rbac\RoleInterface'
             );
         }
 
