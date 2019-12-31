13cd49374441ec2fa74a5c00ad7fbf3a74dfa6ba
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:37:56 -0600

    Rewriting as Laminas Project package

--- src/Assertion/CallbackAssertion.php
+++ src/Assertion/CallbackAssertion.php
@@ -1,18 +1,19 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-permissions-rbac for the canonical source repository
- * @copyright Copyright (c) 2005-2018 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   https://github.com/zendframework/zend-permissions-rbac/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/laminas/laminas-permissions-rbac for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-permissions-rbac/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-permissions-rbac/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace Zend\Permissions\Rbac\Assertion;
+namespace Laminas\Permissions\Rbac\Assertion;
 
-use Zend\Permissions\Rbac\AssertionInterface;
-use Zend\Permissions\Rbac\Exception\InvalidArgumentException;
-use Zend\Permissions\Rbac\Rbac;
-use Zend\Permissions\Rbac\RoleInterface;
+use Laminas\Permissions\Rbac\AssertionInterface;
+use Laminas\Permissions\Rbac\Exception\InvalidArgumentException;
+use Laminas\Permissions\Rbac\Rbac;
+use Laminas\Permissions\Rbac\RoleInterface;
 
 class CallbackAssertion implements AssertionInterface
 {
--- test/RoleTest.php
+++ test/RoleTest.php
@@ -1,18 +1,19 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-permissions-rbac for the canonical source repository
- * @copyright Copyright (c) 2005-2018 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   https://github.com/zendframework/zend-permissions-rbac/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/laminas/laminas-permissions-rbac for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-permissions-rbac/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-permissions-rbac/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace ZendTest\Permissions\Rbac;
+namespace LaminasTest\Permissions\Rbac;
 
+use Laminas\Permissions\Rbac\Exception;
+use Laminas\Permissions\Rbac\Role;
+use Laminas\Permissions\Rbac\RoleInterface;
 use PHPUnit\Framework\TestCase;
-use Zend\Permissions\Rbac\Exception;
-use Zend\Permissions\Rbac\RoleInterface;
-use Zend\Permissions\Rbac\Role;
 
 class RoleTest extends TestCase
 {
