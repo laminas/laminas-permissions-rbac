3b4fab1a011559a71bdecb0d4732c19c290b8735
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:37:34 -0600

    Rewriting as Laminas Project package

--- test/TestAsset/RoleMustMatchAssertion.php
+++ test/TestAsset/RoleMustMatchAssertion.php
@@ -1,20 +1,19 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-permissions-rbac for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-permissions-rbac/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-permissions-rbac/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Permissions\Rbac\TestAsset;
+namespace LaminasTest\Permissions\Rbac\TestAsset;
 
-use Zend\Permissions\Rbac\AbstractRole;
-use Zend\Permissions\Rbac\AssertionInterface;
-use Zend\Permissions\Rbac\Rbac;
+use Laminas\Permissions\Rbac\AbstractRole;
+use Laminas\Permissions\Rbac\AssertionInterface;
+use Laminas\Permissions\Rbac\Rbac;
 
 /**
- * @group      Zend_Rbac
+ * @group      Laminas_Rbac
  */
 class RoleMustMatchAssertion implements AssertionInterface
 {
