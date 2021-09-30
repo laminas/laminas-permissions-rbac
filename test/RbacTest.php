2a247b1d9fbe5edd119ac34e93c06c4a268e83d7
Andi Rückauer <arueckauer@gmail.com>
2021-09-30 14:59:04 +0200

    Remove file headers
    Signed-off-by: Andi Rückauer <arueckauer@gmail.com>

--- test/RbacTest.php
+++ test/RbacTest.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-permissions-rbac for the canonical source repository
- * @copyright https://github.com/laminas/laminas-permissions-rbac/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-permissions-rbac/blob/master/LICENSE.md New BSD License
- */
-
 declare(strict_types=1);
 
 namespace LaminasTest\Permissions\Rbac;
